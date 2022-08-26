b2e5e8c2831bd4086b4e06afaf3c6cb0285bc891
Andrey Potyomkin <and_tem@icloud.com>
2022-08-26 16:40:29 +0300

    bug: Fix command22

--- release.config.js
+++ release.config.js
@@ -4,14 +4,12 @@ module.exports = {
 	"plugins": [
 	[
 		"@semantic-release/commit-analyzer",
-		// {
-		// 	"preset": "conventionalcommits",
-		// 	"releaseRules": [
-		// 		{"type": "bug", "release": "patch"},
-		// 		{"type": "feat", "release": "minor"},
-		// 		{"type": "build", release: false}
-		// 	],
-		// }
+		{
+			"releaseRules": [
+				{"type": "bug", "release": "patch"},
+				{"type": "feat", "release": "minor"}
+			],
+		}
 	],
 	"@semantic-release/release-notes-generator",
 	[
